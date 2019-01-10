c4288618d92c8a9904f0d845b1d210d384878f6b
Filipe Silva <filipematossilva@gmail.com>
2019-01-10 17:07:21 +0000

    add bazel uglify config as well to rollup

--- src/rollup.config.js
+++ src/rollup.config.js
@@ -1,9 +1,40 @@
 
 import nodeResolve from 'rollup-plugin-node-resolve';
 import filesize from 'rollup-plugin-filesize';
-import { terser } from "rollup-plugin-terser";
+import { terser } from 'rollup-plugin-terser';
 import buildOptimizer from '@angular-devkit/build-optimizer/src/build-optimizer/rollup-plugin.js';
 import { sep } from 'path';
+
+const bazelUglifyConfig = {
+  compress: {
+    pure_getters: true,
+    passes: 3,
+    global_defs: {
+      ngDevMode: false
+    },
+    keep_fnames: true,
+    reduce_funcs: true,
+    reduce_vars: true,
+    sequences: true
+  },
+  mangle: true,
+}
+
+const cliUglifyConfig = {
+  safari10: true,
+  output: {
+    ascii_only: true,
+    comments: false,
+    webkit: true,
+  },
+  compress: {
+    pure_getters: true,
+    passes: 3,
+    global_defs: {
+      ngDevMode: false,
+    },
+  },
+}
 
 export default {
   input: './out-tsc/tsc-current/src/main.ngc.js',
@@ -25,21 +56,7 @@ export default {
         `node_modules${sep}rxjs${sep}`,
       ]
     }),
-    terser({
-      safari10: true,
-      output: {
-        ascii_only: true,
-        comments: false,
-        webkit: true,
-      },
-      compress: {
-        pure_getters: true,
-        passes: 3,
-        global_defs: {
-          ngDevMode: false,
-        },
-      }
-    }),
+    terser(cliUglifyConfig),
     filesize(),
   ]
 };
